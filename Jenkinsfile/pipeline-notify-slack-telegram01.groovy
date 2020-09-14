64d4218e6bc7f9d7ee92d1c63d8782f610580087
Leonardo <leonardo.viana@armateus.com.br>
2020-09-14 13:23:23 -0300

    jenkinsNotify

---
+++ Jenkinsfile/pipeline-notify-slack-telegram01.groovy
@@ -0,0 +1,100 @@
+node {
+    try {
+        
+        notifyBuild('STARTED')
+        
+        try{
+            
+        //Stage 1 - Clone do repositorio
+        stage('Clone repositório') { 
+        git branch: 'master',
+                  credentialsId: 'JenkinsInGitlab',
+                  url: 'git@gitlab.mateus:leonardo.viana/deploy-aws-teste.git'
+
+            sh "ls -lat"
+        } 
+        }
+        
+        catch(Exception error) {
+        // Enviar notificação em caso de falha
+        telegramSend(message: "Erro na stage: Clonar repositório :"+error ,chatId:${chattIdTelegram})
+        slackSend (message: "Erro na stage: Clonar repositório :"+error)
+        error "Exception occurred, aborting"
+        }
+        // Enviar notificação de status ok
+        telegramSend(message: "Stage: Clonar repositório\nStatus:OK" ,chatId:chatId:${chattIdTelegram})
+        slackSend (message: "Stage: Clonar repositório\nStatus:OK")
+        
+        try{
+        //Stage 2 - Build com o maven
+        stage ('Build com Maven'){
+                sh 'mvn clean package -DskipTests=true'
+        }
+        }
+        
+        catch(Exception error) {
+        // Enviar notificação em caso de falha
+        telegramSend(message: "Erro na stage: Build com Maven :"+error,chatId:chatId:${chattIdTelegram})
+        slackSend (message: "Erro na stage:  Build com Maven :"+error)
+        error "Exception occurred, aborting"
+        }
+        // Enviar notificação ede status ok
+        telegramSend(message: "Stage: Build com o Maven\nStatus:OK" ,chatId:chatId:${chattIdTelegram})
+        slackSend (message: "Stage: Build com o Maven\nStatus:OK")
+        
+        try{
+        //STage 3 - Teste com o Maven
+        stage ('Testes com o Maven'){
+                sh 'mvn test'
+        }
+        }
+        
+        catch(Exception error) {
+        // Enviar notificação em caso de falha
+        telegramSend(message: "Erro na stage: Testes com o Maven :"+error,chatId:chatId:${chattIdTelegram})
+        slackSend (message: "Erro na stage:  Testes com o Maven :"+error)
+        error "Exception occurred, aborting"
+        }
+        // Enviar notificação ede status ok
+        telegramSend(message: "Stage: Teste com o Maven\nStatus:OK" ,chatId:chatId:${chattIdTelegram})
+        slackSend (message: "Stage: Teste com o maven\nStatus:OK")
+
+  } catch (e) {
+    // Se houver uma excessão o build falha
+    currentBuild.result = "FAILED"
+    throw e
+  } finally {
+    // Sucesso ou falha, sempre enviar notificações
+    notifyBuild(currentBuild.result)
+    
+  }
+}
+
+  def notifyBuild(String buildStatus = 'STARTED') {
+  // Status de Build de nulo significa sucesso
+  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+  // Valores padrã
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}\nNome do Job: '${env.JOB_NAME}' [#${env.BUILD_NUMBER}]"
+  def summary = "${subject}\n(${env.BUILD_URL}consoleText)"
+  // Substitua os valores padrão com base no status da compilação
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESSFUL') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+
+  // Enviar notificação
+  slackSend (color: colorCode, message: summary)
+  telegramSend(message: "${buildStatus}\nNome do Job: '${env.JOB_NAME}'\nNúmero do Build: [#${env.BUILD_NUMBER}]\n(${env.BUILD_URL}consoleText)",chatId:-493170702)
+  
+     
+    
+}
---
+++ Jenkinsfile/pipeline-notify-slack-telegram02.groovy
@@ -0,0 +1,62 @@
+node {
+    try {
+        
+        notifyBuild('STARTED')
+        
+       
+        stage('Clone repositório') { 
+        git branch: 'master',
+                  credentialsId: 'JenkinsInGitlab',
+                  url: 'git@gitlab.mateus:leonardo.viana/deploy-aws-teste.git'
+
+            sh "ls -lat"
+        } 
+       
+        stage ('Build com Maven'){
+                sh 'mvn clean package -DskipTests=true'
+        }
+      
+        stage ('Testes com o Maven'){
+                sh 'mvn test'
+        }
+       
+  } catch (Exception error) {
+    // If there was an exception thrown, the build failed
+    currentBuild.result = "FAILED"
+    telegramSend(message: "Erro :"+error,chatId:chatId:${chattIdTelegram})
+    slackSend (message: "Erro :"+error)
+    error "Exception occurred, aborting"
+    throw error
+  } finally {
+    // Success or failure, always send notifications
+    notifyBuild(currentBuild.result)
+    
+  }
+}
+
+  def notifyBuild(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}\nNome do Job: '${env.JOB_NAME}' [#${env.BUILD_NUMBER}]"
+  def summary = "${subject}\n(${env.BUILD_URL}consoleText)"
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESSFUL') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+  
+   slackSend (color: colorCode, message: summary)
+   telegramSend(message: "${buildStatus}\nNome do Job: '${env.JOB_NAME}'\nNúmero do Build: [#${env.BUILD_NUMBER}]\n(${env.BUILD_URL}consoleText)",chatId:-493170702)
+  
+    
+}
