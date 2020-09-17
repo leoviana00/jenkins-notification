ce60b2af1e1c6ac65ebab0485d73fc5a529ff7c7
Leonardo Viana <leonardoviana00@hotmail.com>
2020-09-17 11:07:54 -0300

    Pipeline - 1 try para bloco de stages

--- Jenkinsfile/pipeline-notify-slack-telegram02.groovy
+++ Jenkinsfile/pipeline-notify-slack-telegram02.groovy
@@ -56,6 +56,5 @@ node {
   }
   
    slackSend (color: colorCode, message: summary)
-   telegramSend(message: "${buildStatus}\nNome do Job: '${env.JOB_NAME}'\nNúmero do Build: [#${env.BUILD_NUMBER}]\n(${env.BUILD_URL}consoleText)",chatId:${chattIdTelegram})
-     
+   telegramSend(message: "${buildStatus}\nNome do Job: '${env.JOB_NAME}'\nNúmero do Build: [#${env.BUILD_NUMBER}]\n(${env.BUILD_URL}consoleText)",chatId:${chattIdTelegram})     
 }
